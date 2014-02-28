cee70303282c074113a90fa29b762ec2849e0899
Russell Barnes <russell.barnes.1@gmail.com>
2014-02-28 00:00:00 -0800

    Init commit of gsm branch

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
+/*#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>*/
 #include "mk20dx128.h"
 #include "core_pins.h"
 #include "usb_serial.h"
+#include "HardwareSerial.h"
 
 
 void TestPrint(char* S){
@@ -9,21 +13,62 @@ void TestPrint(char* S){
 	}
 }
 
+uint8_t gsm_init(void)
+{
+uint8_t success;
+
+// Write several noop commands
+serial_write("AT\r", 3);
+delay(250);
+serial_write("AT\r", 3);
+delay(250);
+serial_write("AT\r", 3);
+delay(250);
+// Clear the serial buffer and write one more noop
+serial_clear();
+// We are expecting to receive "OK"
+success = gsm_write("AT\r", 3);
+
+
+}
+
+uint8_t gsm_write(char *cmd, int len)
+{
+uint8_t available;
+
+delay(100);
+serial_write(cmd, len);
+delay(300);
+available = serial_available();
+if (!available) 
+	return 0;
+
+}
+
 int main(void)
 {
+int success;
 
 pinMode(LED_BUILTIN, OUTPUT);
 
-while(1){
-digitalWriteFast(LED_BUILTIN, HIGH);
-delay(30);
-digitalWriteFast(LED_BUILTIN, LOW);
-delay(3000);
+// Initialize serial
+serial_begin(BAUD2DIV(115200));
+serial_format(SERIAL_8N1);
 
-TestPrint("This is a test! Hi World!\n\r");
+// Initialize GSM module
+gsm_init();
+
+while(1){
+	digitalWriteFast(LED_BUILTIN, HIGH);
+	delay(500);
+	digitalWriteFast(LED_BUILTIN, LOW);
+	delay(500);
+
+	TestPrint("This is a test! Hi World!\n\r");
+	
+	serial_write("This is UART!\r\n", 15);
+	//serial_putchar('n');
 }
 
 return(0);
-
 }
-
