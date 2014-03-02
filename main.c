1a84daa06a32f138809a09fed1615cff2d9b4a52
Russell Barnes <russell.barnes.1@gmail.com>
2014-03-01 17:34:15 -0800

    gsm_write, gsm_init

--- main.c
+++ main.c
@@ -1,74 +1,135 @@
-/*#include <stdio.h> 
-#include <stdlib.h>
-#include <string.h>*/
+#include <stdio.h> 
+//#include <stdlib.h>
+//#include <string.h>
 #include "mk20dx128.h"
 #include "core_pins.h"
 #include "usb_serial.h"
 #include "HardwareSerial.h"
 
+// Accept up to this many characters from the GSM module for each command:
+#define RECEIVE_LIMIT 150
+// Time to wait for a response from the GSM module before a timeout and command failure, in seconds:
+#define COMMAND_TIMEOUT 10
 
-void TestPrint(char* S){
-	while(*S){
-		usb_serial_putchar(*S++);
+
+void USBPrint(char* s){
+	while(*s){
+		usb_serial_putchar(*s++);
 	}
+}
+
+/*
+	gsm_write
+
+	A blocking function that sends a command to the GSM module.
+	Prints the result to the USB serial interface.
+
+	Return values:
+	1: success
+	0: timeout (no response from GSM module)
+	-1: Unexpected response from GSM module
+*/
+uint8_t gsm_write(char *cmd, int len)
+{
+	uint8_t character_counter, timeout_seconds;
+	char received[RECEIVE_LIMIT + 1];
+	char print_status[RECEIVE_LIMIT + len - 1 + 39];
+	char cmd_clean[len - 1];
+	char c;
+	
+	/* 
+		Clear the serial buffer for Serial1 of data before sending the command
+	*/
+	serial_clear();
+	
+	/* 
+		Make a "cleaned-up" version of the command without the
+		\r\n on the end for debug printing:
+	*/
+	snprintf(cmd_clean, len - 1, cmd);
+
+	/* 
+		Send the command and wait for the response.
+		Time-out if it takes more than COMMAND_TIMEOUT seconds.
+	*/
+	serial_write(cmd, len);	// Write the command to the GSM module
+	timeout_seconds = COMMAND_TIMEOUT;
+	while (!serial_available() && timeout_seconds > 0) {
+		delay(1000);//DEBUG
+		timeout_seconds--;
+	}	
+	if (!serial_available()) {
+		// No response
+		snprintf(print_status, RECEIVE_LIMIT + len - 1 + 39, "No response (timed out) for command: %s\r\n", cmd_clean);
+		USBPrint(print_status);
+		return 0;
+	}
+	
+	/*
+		Get response from serial buffer
+	*/
+	character_counter = 0;
+	while (serial_available() && character_counter < RECEIVE_LIMIT) {
+		c = serial_getchar();
+		sprintf(received + character_counter, "%c", c);
+		character_counter++;
+	}
+	
+	/*
+		Parse for "OK" which is the module's success message
+	*/
+	if (received[0] != 'O' || received [1] != 'K') {
+		// Unexpected response
+		snprintf(print_status, RECEIVE_LIMIT + len - 1 + 37, "Unexpected response for command %s: %s\r\n", cmd_clean, received);
+		USBPrint(print_status);
+		return 0;
+	}
+	
+	/*
+		Print result to USB serial
+	*/
+	snprintf(print_status, RECEIVE_LIMIT + len - 1 + 26, "Response for command %s: %s\r\n", cmd_clean, received);
+	USBPrint(print_status);
+	return 1;
 }
 
 uint8_t gsm_init(void)
 {
-uint8_t success;
-
-// Write several noop commands
-serial_write("AT\r", 3);
-delay(250);
-serial_write("AT\r", 3);
-delay(250);
-serial_write("AT\r", 3);
-delay(250);
-// Clear the serial buffer and write one more noop
-serial_clear();
-// We are expecting to receive "OK"
-success = gsm_write("AT\r", 3);
-
-
-}
-
-uint8_t gsm_write(char *cmd, int len)
-{
-uint8_t available;
-
-delay(100);
-serial_write(cmd, len);
-delay(300);
-available = serial_available();
-if (!available) 
-	return 0;
-
+	// Write several noop commands
+	serial_write("AT\r\n", 4);
+	delay(250);
+	serial_write("AT\r\n", 4);
+	delay(250);
+	serial_write("AT\r\n", 4);
+	delay(250);
+	// Write one more noop; we are expecting to receive "OK"
+	return gsm_write("AT\r\n", 4);
 }
 
 int main(void)
 {
-int success;
+	int success;
 
-pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 
-// Initialize serial
-serial_begin(BAUD2DIV(115200));
-serial_format(SERIAL_8N1);
+	// Initialize serial
+	serial_begin(BAUD2DIV(115200));
+	serial_format(SERIAL_8N1);
 
-// Initialize GSM module
-gsm_init();
-
-while(1){
-	digitalWriteFast(LED_BUILTIN, HIGH);
-	delay(500);
-	digitalWriteFast(LED_BUILTIN, LOW);
-	delay(500);
-
-	TestPrint("This is a test! Hi World!\n\r");
+	// Initialize GSM module
+	//gsm_init();
 	
-	serial_write("This is UART!\r\n", 15);
-	//serial_putchar('n');
-}
+	// Send set-up commands
 
-return(0);
+	while(1){
+		digitalWriteFast(LED_BUILTIN, HIGH);
+		delay(500);
+		digitalWriteFast(LED_BUILTIN, LOW);
+		delay(500);
+
+		success = gsm_init();
+		delay(1500);
+	}
+
+	return(0);
 }
