686e76b672c660de073d367fc985fbbc8bc4b729
Russell Barnes <russell.barnes.1@gmail.com>
2014-03-02 00:04:39 -0800

    Restore main file to previous state

--- main.c
+++ main.c
@@ -1,51 +1,29 @@
-#include "gsm.h"
+#include "mk20dx128.h"
+#include "core_pins.h"
+#include "usb_serial.h"
 
-/*
-	gsm_example.c
 
-	Example code for gsm.c
-
-	This code should be run simultaneously to or after 
-	powering on the GSM Click module.
-
-	Uses Serial1 on the Teensy3.1
-*/
+void TestPrint(char* S){
+	while(*S){
+		usb_serial_putchar(*S++);
+	}
+}
 
 int main(void)
 {
-	int success;
-	
-	pinMode(LED_BUILTIN, OUTPUT);
-	
-	// Initialize Serial1
-	serial_begin(BAUD2DIV(115200));
-	serial_format(SERIAL_8N1);
-	
-	// Initialize GSM module
-	success = gsm_init();
-	
-	// Send an SMS message
-	if (success > 0)
-		success = gsm_send_sms("1503#######", "Sent from the Teensy.");
-	/*
-	Alternative method: 
-	if (success > 0)
-		success = gsm_write("AT+CMGF=1\r\n", 11);
-	if (success > 0)
-		success = gsm_write("AT+CMGS=\"1503#######\"\r\n", 23);
-	if (success > 0)
-		success = gsm_write("Sent from the Teensy\x1a", 21);
-	*/
-	
-	while(1){
-		digitalWriteFast(LED_BUILTIN, HIGH);
-		delay(500);
-		digitalWriteFast(LED_BUILTIN, LOW);
-		delay(500);
-		
-		if (success > 0)
-			USBPrint("SMS message transmission succeeded\r\n");
-	}
-	
-	return(0);
+
+pinMode(LED_BUILTIN, OUTPUT);
+
+while(1){
+digitalWriteFast(LED_BUILTIN, HIGH);
+delay(30);
+digitalWriteFast(LED_BUILTIN, LOW);
+delay(3000);
+
+TestPrint("This is a test! Hi World!\n\r");
 }
+
+return(0);
+
+}
+
