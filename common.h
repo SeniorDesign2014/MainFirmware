d6bd103ddd6c15bc2c3ce6df311cbfc0919b7ea3
burrisp <burrisp@onid.orst.edu>
2014-03-06 21:33:08 -0800

    Added files from bluetooth branch

--- common.h
+++ common.h
@@ -5,52 +5,6 @@
  extern "C" {
  #endif
  
-	
-	/*
-	extern signed int ACCEL_XOUT;
-	extern signed int ACCEL_YOUT;
-	extern signed int ACCEL_ZOUT;
-	extern float GYRO_XRATE;
-	extern float GYRO_YRATE;
-	extern float GYRO_ZRATE;
-	extern int GYRO_XRATERAW;
-	extern int GYRO_YRATERAW;
-	extern int GYRO_ZRATERAW;
-
-	extern unsigned char GYRO_XOUT_L;
-	extern unsigned char GYRO_XOUT_H;
-	extern unsigned char GYRO_YOUT_L;
-	extern unsigned char GYRO_YOUT_H;
-	extern unsigned char GYRO_ZOUT_L;
-	extern unsigned char GYRO_ZOUT_H;
-	extern signed int GYRO_XOUT;
-	extern signed int GYRO_YOUT;
-	extern signed int GYRO_ZOUT;	
-
-	extern unsigned char ACCEL_XOUT_L;
-	extern unsigned char ACCEL_XOUT_H;
-	extern unsigned char ACCEL_YOUT_L;
-	extern unsigned char ACCEL_YOUT_H;
-	extern unsigned char ACCEL_ZOUT_L;
-	extern unsigned char ACCEL_ZOUT_H;
-
-	extern signed long GYRO_XOUT_OFFSET_1000SUM;
-	extern signed long GYRO_YOUT_OFFSET_1000SUM;
-	extern signed long GYRO_ZOUT_OFFSET_1000SUM;
-	extern signed long GYRO_XOUT_OFFSET;
-	extern signed long GYRO_YOUT_OFFSET;
-	extern signed long GYRO_ZOUT_OFFSET;
-
-	extern float GYRO_XANGLE;
-	extern float GYRO_YANGLE;
-	extern float GYRO_ZANGLE;
-	extern long GYRO_XANGLERAW;
-	extern long GYRO_YANGLERAW;
-	extern long GYRO_ZANGLERAW;
-	extern float ACCEL_XANGLE;
-	extern float ACCEL_YANGLE;
-	extern float ACCEL_ZANGLE;
-	*/
 	void simplePrint(char *S);
 	
 #ifdef __cplusplus
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+/**********************************
+main.cpp
+Created: 03/06/2014
+Authors: Paul Burris, Nick Voigt, Russell Barnes
 
+This is the integrated file.
+**********************************/
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
@@ -7,57 +13,149 @@
 #include "usb_serial.h"
 #include "bluetooth.h"
 #include "HardwareSerial.h"
+#include "MPU6050.h"
 //this is a common header for functions used in several c files (ex. simpleprint())
 #include "common.h"		
-#include "MPU6050.h"
-
-//i2c variables
-uint8_t target = 0x68;
-uint8_t who_am_i = 0x75;
-
-//bluetooth variables
-extern int bt_write_flag;
-extern int bt_whitelist_flag;
-extern int bt_set_mode_flag;
-extern int bt_connected_flag;
-extern int bt_armed;
-extern int bt_sound;
-extern int bt_sound_select;
-extern int bt_sound_delay;
-extern int bt_new_data;
 
 //state varaibales
+int state = 0;
 char alarmed = '0';
 char armed = '0';
 char secret = '0';
 char sound = '0';
 char sound_sel = '0';
 char sound_delay = '0';
+enum states {
+	STATE_DISARMING,
+	STATE_DISARMED,
+	STATE_ARMING,
+	STATE_ARMED,
+	STATE_ALARMING,
+	STATE_ALARMED
+}
 
+//varaibles to use usb serial debugging (115200 baud)
 char debug_command = 0;
 char debug_output[255];
 
-/* DECIDED TO GO WITH SPRINTF
-void PrettyFormatInt( int i, char* buf, int buflen ){
-  // don't forget ends:
-  ostrstream temp( buf, buflen );
-  temp << setw(4) << i << ends;
-}
-
-void PrettyFormatFloat(float i, std::string *s){
-	std::ostringstream ss;
-	ss << i;
-	*s(ss.str());
-}
-*/
-
 int main(void){
 	pinMode(LED_BUILTIN, OUTPUT);
-	//bluetooth_init();
+	bluetooth_init();
 	
 	//bluetooth_set_mode(BT_GENERAL_DISCOVERABLE, BT_UNDIRECTED_CONNECTABLE);
 	
 	for(;;){
+		switch(state){
+		
+			case STATE_DISARMING:
+				//TODO: shut down GPS
+				//TODO: shut down GSM
+				//TODO: shut down audio
+				state = STATE_DISARMED;
+				
+			break;
+			case STATE_DISARMED:
+				//poll bluetooth every so often
+				bluetooth_update();
+				if(bt_new_data == 1){
+					armed = bt_armed + ASCII;
+					sound = bt_sound + ASCII;
+					sound_sel = bt_sound_select + ASCII;
+					sound_delay = bt_sound_delay + ASCII;
+					
+					bt_new_data = 0;
+				}
+				if(armed == '1'){state = STATE_ARMING;}
+				
+				//TODO: sleep
+			break;
+			
+			case STATE_ARMING:
+				//TODO: GPS init
+				//TODO: motion init
+				state = STATE_ARMED;
+			break;
+			
+			case STATE_ARMED:
+				//TODO: ADD poll GPS until lock has been established
+				/*
+				if(gps_lock != true){
+					update_gps();
+					if(gps_lock == true){
+						sleep GPS;
+					}
+				}
+				*/
+				
+				//TODO: timer (with int)
+				
+				//poll the motion sensor every .5 s
+				alarmed = motion_update();
+				if(alarmed == '1'){
+					state = STATE_ALARMING;
+				}
+				
+				//poll bluetooth every so often
+				bluetooth_update();
+				if(bt_new_data == 1){
+					armed = bt_armed + ASCII;
+					sound = bt_sound + ASCII;
+					sound_sel = bt_sound_select + ASCII;
+					sound_delay = bt_sound_delay + ASCII;
+					
+					bt_new_data = 0;
+				}
+				if(armed == '0'){state = STATE_DISARMING;}
+			break;
+			
+			case STATE_ALARMING:
+				//TODO: GSM init
+				//TODO: wake up GPS
+				//TODO: fire up GSM
+				
+				state = STATE_ALARMED;
+				
+			break;
+			
+			case STATE_ALARMED:
+				simplePrint("FREAKOUT! I'M BEING STOLEN!\n");
+				
+				//TODO: get GPS data
+				//gps_parse();
+				//TODO: send GSM data 
+				
+				//poll bluetooth for disarm message
+				bluetooth_update();
+				if(bt_new_data == 1){
+					armed = bt_armed + ASCII;
+					sound = bt_sound + ASCII;
+					sound_sel = bt_sound_select + ASCII;
+					sound_delay = bt_sound_delay + ASCII;
+					
+					bt_new_data = 0;
+				}
+				if(armed == '0'){state = STATE_DISARMING;}
+				
+			break;
+			
+			default:
+				simplePrint("ERROR - INCORRECT STATE\n");
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	//THIS IS MY MANUAL DEBUG CODE
 		if(usb_serial_available()){
 			debug_command = usb_serial_getchar();
 		}
@@ -65,22 +163,6 @@ int main(void){
 		digitalWriteFast(LED_BUILTIN, LOW);
 		delay(30);
 		digitalWriteFast(LED_BUILTIN, HIGH);
-		
-		if(bt_new_data == 1){
-			armed = bt_armed + ASCII;
-			sound = bt_sound + ASCII;
-			sound_sel = bt_sound_select + ASCII;
-			sound_delay = bt_sound_delay + ASCII;
-			
-			bt_new_data = 0;
-		}
-		
-		if(armed == '1' && alarmed == '0'){
-			alarmed = motion_update();
-		}
-		if(alarmed == '1'){
-			simplePrint("FREAKOUT! I'M BEING STOLEN!\n");
-		}
 		
 		//bluetooth_update();
 		if(debug_command == 'b'){
@@ -133,38 +215,6 @@ int main(void){
 				simplePrint("System Armed.\n");
 				motion_arm_position();
 			}
-			
-			/*
-			motion_get_gyro_values();
-			sprintf(debug_output, "Gyro X: %f ", GYRO_XANGLE);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Y: %f ", GYRO_YANGLE);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Z: %f ", GYRO_ZANGLE);
-			simplePrint(debug_output);
-			
-			sprintf(debug_output, "X_OUT: %i ", GYRO_XOUT);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Y_OUT: %i ", GYRO_YOUT);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Z_OUT: %i\n", GYRO_ZOUT);
-			simplePrint(debug_output);
-			
-			motion_get_accel_values();
-			sprintf(debug_output, "Accel X: %f ", ACCEL_XANGLE);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Y: %f ", ACCEL_YANGLE);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Z: %f ", ACCEL_ZANGLE);
-			simplePrint(debug_output);
-			
-			sprintf(debug_output, "X_OUT: %i ", ACCEL_XOUT);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Y_OUT: %i ", ACCEL_YOUT);
-			simplePrint(debug_output);
-			sprintf(debug_output, "Z_OUT: %i\n", ACCEL_ZOUT);
-			simplePrint(debug_output);
-			*/
 			debug_command = 0;
 		}
 		
